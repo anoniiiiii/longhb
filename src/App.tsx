5cc47011bba23fbfece105ab2f801063bc0bcba6
Huỳnh Long <longhb@Huynhs-MacBook-Pro.local>
2020-06-16 14:08:13 +0700

    Remove CV Page on route

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
 import React from "react";
 import "./App.less";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import CVPage from "../pages/cv";
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/cv">
-            <CVPage></CVPage>
-          </Route>
-        </Switch>
-      </Router>
     </div>
   );
 }
