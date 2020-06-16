9cf4e5504e00a797f7055bc9afb419233a5e0348
Huỳnh Long <longhb@Huynhs-MacBook-Pro.local>
2020-06-16 14:20:30 +0700

    Add server less

--- next.config.js
+++ next.config.js
@@ -2,12 +2,17 @@
 // This source was taken from the @next/mdx plugin source:
 // https://github.com/zeit/next.js/tree/canary/packages/next-mdx
 module.exports = {
+  target: 'serverless',
   webpack: (config, options) => {
     config.module.rules.push({
       test: /\.(png|jpe?g|gif)$/i,
       loader: "url-loader",
     });
 
+    config.module.rules.push({
+      test: /\.md$/,
+      use: 'raw-loader',
+    })
     return config;
   },
 };
